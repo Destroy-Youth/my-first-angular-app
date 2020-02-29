b4774caef0ef429e59142f0e8f32ed78c76ae6b3
Ivan Equihua <luis.herrerae@outlook.com>
2020-02-29 13:27:24 -0600

    task 2: Data and event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+  id = 10;
+  status = 'on standby';
+  allowAddServer = false;
+  serverCreationStatus = 'No server has been created';
+  serverName: string;
+  userName: string;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = this.serverName.concat(' server created!');
+    alert(this.serverCreationStatus);
+  }
+
+  deleteUserName() {
+    this.userName = '';
   }
 
 }
