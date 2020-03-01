1f329cc7e2067abd22797fe24df05d9b8caa3952
Ivan Equihua <luis.herrerae@outlook.com>
2020-03-01 13:10:34 -0600

    Changing components location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserNameComponent } from './user-name/user-name.component';
+import { WarningAlertComponent } from './exercises/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './exercises/success-alert/success-alert.component';
+import { ServerComponent } from './exercises/server/server.component';
+import { ServersComponent } from './exercises/servers/servers.component';
+import { UserNameComponent } from './exercises/user-name/user-name.component';
 import { DetailsComponent } from './details/details.component';
-import { ExampleExerciseComponent } from './example-exercise/example-exercise.component';
+import { ExampleExerciseComponent } from './exercises/example-exercise/example-exercise.component';
 
 
 @NgModule({
